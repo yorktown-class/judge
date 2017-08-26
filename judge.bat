15801d10f7191d3d9635fde056488228b12c7cce
yanghong <1484944458@qq.com>
2017-08-26 16:12:38 +0800

    newpro

---
+++ judge.bat
@@ -0,0 +1,29 @@
+@echo off
+
+echo "Problem ID:"
+set /p div=""
+echo "File Name"
+set /p nm=""
+
+g++ %nm%.cpp -o tmp\%nm% 
+
+for %%I in (Data\%div%\*.in) do (
+	echo -------------------- 
+	echo Runing on Test %%~nI
+	echo --------------------
+
+	tmp\%nm%  < Data\%div%\%%~nI.in > tmp\test.out
+
+	fc Data\%div%\%%~nI.out tmp\test.out
+	if errorlevel == 1 (
+		echo GG!!!
+		pause
+		goto :loop
+	)
+	echo Accepted Test %%~nI
+)
+echo Accepted!!!
+pause
+:loop
+del tmp\test.out
+del tmp\%nm%.exe
